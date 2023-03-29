35859e6d9a2f4a63e4f84f3ac1b37b1f3898c67f
Benjamin Li <benli03@utexas.edu>
2023-03-28 21:45:34 -0500

    hukuhk

---
+++ Li_Benjamin _HW4/Li_Benjamin _HW4/Models/AppUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+
+namespace Li_Benjamin_HW4.Models
+{
+	public class AppUser : IdentityUser
+	{
+		[Display(Name = "First Name")]
+		public String FirstName { get; set; }
+
+		[Display(Name = "Last Name")]
+		public String LastName { get; set; }
+	}
+}
+
---
+++ Li_Benjamin _HW4/Li_Benjamin _HW4/Models/Product.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Li_Benjamin_HW4.Models
+{
+	public enum ProductType
+	{
+		[Display(Name = "New Hardback")] New_Hardback,
+		[Display(Name = "New Paperback")] New_Paperback,
+		[Display(Name = "Used Hardback")] Used_Hardback,
+		[Display(Name = "Used Paperback")] Used_Paperback,
+		Other
+	}
+
+	public class Product
+	{
+		public Int32 ProductID { get; set; }
+
+		[Required(ErrorMessage = "Product name is required!")]
+		[Display(Name = "Product Name:")]
+		public String Name { get; set; }
+
+		[Display(Name = "Product Description:")]
+		public String? Description { get; set; }
+
+		[Required(ErrorMessage = "Product price is required!")]
+		[Display(Name = "Product Price:")]
+		[DisplayFormat(DataFormatString = "{0:c}")]
+		public Decimal Price { get; set; }
+
+		[Display(Name = "Product Type:" )]
+		public ProductType ProductType { get; set; }
+	}
+}
+
